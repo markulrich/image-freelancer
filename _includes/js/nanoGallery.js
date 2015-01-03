2dd83173372c1accb1ad3d77ac2456cf9e22c25b
Mark Ulrich <mulrich@stanford.edu>
2015-01-02 22:13:02 -1000

    Cleaned up comments

--- _includes/js/nanoGallery.js
+++ _includes/js/nanoGallery.js
@@ -1,22 +1,5 @@
 $(function () {
 
-  var myColorScheme = {
-    //navigationbar: {
-    //  background: '#f0f',
-    //  border: '1px dotted #555',
-    //  color: '#ccc',
-    //  colorHover: '#fff'
-    //},
-    thumbnail: {
-      background: 'rgba(1, 1, 1, 0.0)',
-      border: '0px solid #000'
-      //labelBackground: 'transparent',
-      //labelOpacity: '0.8',
-      //titleColor: '#fff',
-      //descriptionColor: '#eee'
-    }
-  };
-
   $(".nanoGallery").nanoGallery({
     kind: 'flickr',
     userID: '130203367@N06',
@@ -24,7 +7,15 @@ $(function () {
     thumbnailHeight: 'auto',
     photoset: '72157649845564122',
     thumbnailHoverEffect: [{name: 'labelAppear75', duration: 1000}],
-    colorScheme: myColorScheme,
+    colorScheme: {
+      thumbnail: {
+        background: 'rgba(1, 1, 1, 0.0)',
+        border: '0px solid #000'
+      }
+    },
+    colorSchemeViewer: {
+      background: '#000'
+    },
     viewerToolbar: {
       autoMinimize: 2000,
       standard: 'customN, minimizeButton, previousButton, pageCounter, nextButton, playPauseButton, fullscreenButton, infoButton, linkOriginalButton, closeButton, label'
@@ -34,29 +25,4 @@ $(function () {
     i18n: {thumbnailImageDescription: 'View Photo'},
     thumbnailLabel: {display: true, position: 'overImageOnMiddle', align: 'center'}
   });
-  var myColorSchemeViewer = {
-    background: 'rgba(1, 1, 1, 0.75)'//,
-    //imageBorder: '15px solid #f8f8f8',
-    //imageBoxShadow: '#888 0px 0px 20px',
-    //barBackground: '#222',
-    //barBorder: '2px solid #111',
-    //barColor: '#eee',
-    //barDescriptionColor: '#aaa'
-  };
-  //
-  //jQuery(".nanoGallery").nanoGallery({
-  //  kind: 'flickr',
-  //  userID: '130203367@N06',
-  //  thumbnailWidth: 'auto',
-  //  thumbnailHeight: 200,
-  //  photoset: '72157649845564122',
-  //  thumbnailHoverEffect: [{name: 'labelAppear75', duration: 300}],
-  //  thumbnailHoverEffect: 'borderLighter',
-  //  colorScheme: myColorScheme,
-  //  thumbnailGutterWidth: 0,
-  //  thumbnailGutterHeight: 0,
-  //  i18n: {thumbnailImageDescription: 'View Photo'},
-  //  thumbnailLabel: {display: true, position: 'overImageOnMiddle', align: 'center'},
-  //  colorSchemeViewer: myColorSchemeViewer
-  //});
 });
